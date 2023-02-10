312d885c8f608548cab004326d816c4e5a48b3e8
keilander <keilander@osii.com>
2023-02-10 13:16:36 -0600

    add comparison for date ages

--- age.py
+++ age.py
@@ -19,3 +19,49 @@ def age(d:typing.Union[datetime.timedelta,datetime.datetime])->datetime.timedelt
     if not isinstance(d,datetime.timedelta):
         d=datetime.datetime.now()-d
     return d
+
+
+def ageCmp(
+    a:typing.Union[datetime.datetime,datetime.timedelta],
+    b:typing.Union[datetime.datetime,datetime.timedelta]
+    )->datetime.timedelta:
+    """
+    compare two date ages and return a timedelta
+    """
+    return age(a)-age(b)
+
+
+def minAge(
+    a:typing.Union[None,datetime.datetime,datetime.timedelta],
+    b:typing.Union[None,datetime.datetime,datetime.timedelta]
+    )->typing.Union[None,datetime.datetime,datetime.timedelta]:
+    """
+    a min() function for date ages
+    """
+    if a is None:
+        result=b
+    elif b is None:
+        result=a
+    elif age(a)<age(b):
+        result=a
+    else:
+        result=b
+    return result
+
+
+def maxAge(
+    a:typing.Union[None,datetime.datetime,datetime.timedelta],
+    b:typing.Union[None,datetime.datetime,datetime.timedelta]
+    )->typing.Union[None,datetime.datetime,datetime.timedelta]:
+    """
+    a max() function for date ages
+    """
+    if a is None:
+        result=b
+    elif b is None:
+        result=a
+    elif age(a)>age(b):
+        result=a
+    else:
+        result=b
+    return result
